29f98ae20b61e529d54a5119ebb17d0d6e119d54
jpmacias <jpmacias@cobiscorp.com>
2024-09-08 10:12:37 -0500

    rama-jpm-feat: Ajuste para reseteo contraseña

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,9 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-       $this->call([
-           EstadosSeeder::class,
-           TiposIdentificacionSeeder::class,
-       ]);
+        $this->call([
+            EstadosSeeder::class,
+            TiposIdentificacionSeeder::class,
+            ConfigServerSeeder::class,
+        ]);
     }
 }
