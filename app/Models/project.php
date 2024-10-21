ecfa0981f6d9cd24751e1a138cca35a876ec2339
HamiltonDiaz <54079326+HamiltonDiaz@users.noreply.github.com>
2024-10-21 16:08:36 -0500

    feat: se implementa editar proyectos

--- app/Models/project.php
+++ app/Models/project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class project extends Model
 {
@@ -20,6 +21,30 @@ class project extends Model
         'id_estado'
     ];
     
+    // Mutator para formatear 'fechainicio'
+    public function setFechainicioAttribute($value)
+    {
+        if (!is_null($value)) {
+            // Convertimos el valor en una instancia de Carbon y luego lo formateamos como 'Y-m-d'
+            $this->attributes['fechainicio'] = Carbon::parse($value)->format('Y-m-d');
+        } else {
+            // Si el valor es null, lo asignamos directamente
+            $this->attributes['fechainicio'] = null;
+        }
+    }
+
+    // Mutator para formatear 'fechafin'
+    public function setFechafinAttribute($value)
+    {
+        if (!is_null($value)) {
+            // Convertimos el valor en una instancia de Carbon y luego lo formateamos como 'Y-m-d'
+            $this->attributes['fechafin'] = Carbon::parse($value)->format('Y-m-d');
+        } else {
+            // Si el valor es null, lo asignamos directamente
+            $this->attributes['fechafin'] = null;
+        }
+    }
+
 
     // Definir las relaciones
     public function cateogria()
